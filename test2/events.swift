a805d2efdbe6f84008ae68904cd29adea54b683a
Sabrina <34215580+BwinaRose@users.noreply.github.com>
2020-02-08 14:42:00 -0500

    second commit

--- test2/events.swift
+++ test2/events.swift
@@ -11,9 +11,12 @@ import Foundation
 
 var events: [[String:Any]] =
     [
+        ["image": "op", "title": "Software Engineering Oppurtunities", "time": "Jan 26 - Mar 6", "location": "TBA", "description": ""],
         ["image": "teal", "title": "Teal Program", "time": "February 11 (11am-12pm)", "location": "TBA", "description": ""],
         ["image": "codeswarm", "title": "Code Swarm", "time": "February 7 - 8", "location": "BOA 309", "description": "fun and games, cool prizes, awesome people!"],
+        ["image": "deltech", "title": "Software Engineering Programs", "time": "Feb 6 (11am-12pm)", "location": "SC 333", "description": ""],
         ["image": "makerspace", "title": "Maker Space", "time": "January 31 (10am-12pm, 2pm-4pm)", "location": "SC 144", "description": "Join your fellow department for a tour of all the awesome stuff we have!"]
+
         
         
 
